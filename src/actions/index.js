b524710aea6e0d4440b397b08e89e52aa2d9d1aa
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 06:14:58 -0400

    Added actions/actions.js, with clearHistory action

---
+++ src/actions/index.js
@@ -0,0 +1,16 @@
+import {
+  SHOW_HISTORY, CLEAR_HISTORY,
+  SELECT_HISTORY_ITEM, PRESS_EQUALS,
+  PRESS_CLEAR, PRESS_DIGIT,
+  PRESS_DECIMAL, PRESS_OPERATION
+} from "../constants/actionTypes.js";
+
+export const clearHistory = () => ({type: CLEAR_HISTORY});
+
+
+
+
+
+
+
+
