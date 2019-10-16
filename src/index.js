c20c468ac95ba668a176b3961912a800c0be5180
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 06:30:18 -0400

    Created initial state, store, and added Provider.

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './assets/css/style.scss';
 import App from './components/App';
+import rootReducer from './reducers';
 
 import * as serviceWorker from './serviceWorker';
 
-render(<App />, document.getElementById('app'));
+const initialState = {
+  history: []
+};
+
+const store = createStore(rootReducer, initialState);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('app')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
