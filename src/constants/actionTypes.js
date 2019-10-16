a71b95683f2900e6fd715e6b70e8d91d7a752f7c
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 06:05:12 -0400

    Added actionTypes constants

---
+++ src/constants/actionTypes.js
@@ -0,0 +1,8 @@
+export const SHOW_HISTORY = 'SHOW_HISTORY';
+export const CLEAR_HISTORY = 'CLEAR_HISTORY';
+export const SELECT_HISTORY_ITEM = 'SELECT_HISTORY_ITEM';
+export const PRESS_EQUALS = 'PRESS_EQUALS';
+export const PRESS_CLEAR = 'PRESS_CLEAR';
+export const PRESS_DIGIT = 'PRESS_DIGIT';
+export const PRESS_DECIMAL = 'PRESS_DECIMAL';
+export const PRESS_OPERATION = 'PRESS_OPERATION';
