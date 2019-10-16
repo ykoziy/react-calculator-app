9ae006cba4bb97920734ff0de2e615c5ebb110e3
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 11:27:01 -0400

    OutputDisplay is now a container

---
+++ src/containers/OutputDisplay.js
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+
+const OutputDisplay = ({result}) => {
+    let output = result;
+    if(result === "")
+    {
+        output = "0"
+    }
+    return(
+        <div id="display">{output}</div>
+    );
+}
+OutputDisplay.propTypes = {
+    result: PropTypes.any.isRequired
+};
+
+const mapStateToProps = (state) => ({
+    result: state.result
+});
+
+export default connect(
+    mapStateToProps
+)(OutputDisplay);
