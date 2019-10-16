0c26ad3750febb80ab881283272e2c0fe9dddbac
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 15:19:01 -0400

    Moved HistoryItem to containers, fixed import in History container.

--- src/containers/History.js
+++ src/containers/History.js
@@ -1,8 +1,8 @@
 import React from "react";
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
-import HistoryItem from "../components/HistoryItem";
 import HistoryBody from "../components/HistoryBody";
+import HistoryItem from "./HistoryItem";
 import HistoryFooter from "./HistoryFooter";
 
 const History = ({history}) => {
