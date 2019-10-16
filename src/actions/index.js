6f0b815f09946baeb51a421e665b292e8f89571d
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 11:48:48 -0400

    Added actions: pressEquals, pressDecimal, pressOperation, pressClear

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,11 +7,7 @@ import {
 
 export const clearHistory = () => ({type: CLEAR_HISTORY});
 export const pressDigit = (obj) => ({type: PRESS_DIGIT, obj});
-
-
-
-
-
-
-
-
+export const pressEquals = (obj) => ({type: PRESS_EQUALS, obj});
+export const pressDecimal = (obj) => ({type: PRESS_DECIMAL, obj});
+export const pressOperation = (obj) => ({type: PRESS_OPERATION, obj});
+export const pressClear = (obj) => ({type: PRESS_CLEAR, obj});
