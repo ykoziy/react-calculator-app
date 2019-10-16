6a15b57261c9a7ce6bc8d8df1cd914d2f1c2625c
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 15:16:46 -0400

    Refactored App component

--- src/components/App.js
+++ src/components/App.js
@@ -8,37 +8,11 @@ import ControlsView from "../containers/ControlsView";
 
 class App extends Component {
   render() {
-    return (
-      <CalculatorApp />
-    );
+    return <CalculatorApp />;
   }
 }
 
 class CalculatorApp extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            formula: "",
-            evaluated: false,
-            previousResult: "",
-            result: "",
-            isHistoryView: false,
-            history: []
-        };
-    }
-
-    handleHistoryClick = (event) => {
-        let expression = event.currentTarget.firstChild.textContent;
-        let result = event.currentTarget.lastChild.textContent
-        this.setState(prevState => ({
-            formula: expression,
-            evaluated: true,
-            previousResult: result,
-            result: result,
-            isHistoryView: !prevState.isHistoryView
-        }));
-    }
-
     render() {
         return (
           <div id="calculator">
