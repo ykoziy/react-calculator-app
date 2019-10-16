74e124fc730ac95449647ecd62e71d24cf3042a3
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 15:10:11 -0400

    Connected History to redux store, added event handler.

--- src/components/HistoryItem.js
+++ src/components/HistoryItem.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { selectHistoryItem } from '../actions'
 
-const HistoryItem = ({expression, result, handleHistoryClick}) => {
+const HistoryItem = ({expression, result, selectHistoryItem}) => {
+    const handleItemClick = () => {
+        selectHistoryItem({
+            formula: expression,
+            previousResult: result,
+            result: result,
+        });
+    }
+
     return (
-        <div className="history-item" onClick={handleHistoryClick}>
+        <div className="history-item" onClick={handleItemClick}>
             <div className="history-item-expression">
                 {expression}
             </div>
@@ -13,10 +23,18 @@ const HistoryItem = ({expression, result, handleHistoryClick}) => {
         </div>
     );
 }
+
 HistoryItem.propTypes = {
     expression: PropTypes.string.isRequired,
     result: PropTypes.string.isRequired,
-    handleHistoryClick: PropTypes.func.isRequired
+    selectHistoryItem: PropTypes.func.isRequired
 };
 
-export default HistoryItem;
+const mapDispatchToProps = (dispatch) => ({
+    selectHistoryItem: (obj) => dispatch(selectHistoryItem(obj))
+});
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(HistoryItem);
