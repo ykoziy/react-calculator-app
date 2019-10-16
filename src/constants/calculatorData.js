bc1c909308a9b46feb0fe192ca82c394fe48ff19
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 10:13:10 -0400

    Added type number, to stop relying on undefined type

--- src/constants/calculatorData.js
+++ src/constants/calculatorData.js
@@ -18,42 +18,52 @@ export const calculatorButtons = [
     },
     {
         id:"seven",
+        type:"number",
         text:"7"
     },
     {
         id:"eight",
+        type:"number",
         text:"8"
     },
     {
         id:"nine",
+        type:"number",
         text:"9"
     },
     {
         id:"four",
+        type:"number",
         text:"4"
     },
     {
         id:"five",
+        type:"number",
         text:"5"
     },
     {
         id:"six",
+        type:"number",
         text:"6"
     },
     {
         id:"one",
+        type:"number",
         text:"1"
     },
     {
         id:"two",
+        type:"number",
         text:"2"
     },
     {
         id:"three",
+        type:"number",
         text:"3"
     },
     {
         id:"zero",
+        type:"number",
         text:"0"
     },
     {
