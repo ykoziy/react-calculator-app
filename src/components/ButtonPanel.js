ce59a8204af6223678f549104551a97d7973bcbc
Yuriy Koziy <yukoziy@gmail.com>
2019-10-16 09:24:43 -0400

    Refactored ButtonPanel, removed PropTypes and variable eventHandler

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,11 +1,9 @@
 import React from "react";
-import PropTypes from 'prop-types';
 import { calculatorButtons } from "../constants/calculatorData";
 import Button from "./Button";
 
-const ButtonPanel = (props) => {
+const ButtonPanel = () => {
     let buttons = calculatorButtons.map((i, idx) => {
-        let eventHandler;
         return <Button key={"btn-"+idx} id={i.id} type={i.type} text={i.text} />
     });
     return(
@@ -14,8 +12,5 @@ const ButtonPanel = (props) => {
         </div>
     );
 }
-ButtonPanel.propTypes = {
-    children: PropTypes.arrayOf(PropTypes.element).isRequired
-};
 
 export default ButtonPanel;
