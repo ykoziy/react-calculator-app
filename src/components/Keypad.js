3f531d680ba6593eec3a040eeaa04c0a8f623931
Yuriy Koziy <yukoziy@gmail.com>
2019-10-18 05:53:25 -0400

    Added OperationButton container

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -2,29 +2,32 @@ import React from "react";
 import { calculatorButtons } from "../constants/calculatorData";
 import Button from "../containers/Button";
 import DigitButton from "../containers/DigitButton";
+import OperationButton from "../containers/OperationButton";
 
 const ButtonModule = (idx, id, type, text) => {
     const digitButton = () => {
         return <DigitButton key={"btn-"+idx} id={id} type={type} text={text} />
     }
     const operationButton = () => {
-        //return <OperationButton key={"btn-"+idx} id={id} type={type} text={text} />
-        return "WIP"
+        return <OperationButton key={"btn-"+idx} id={id} type={type} text={text} />
     }
 
     return {
         digitButton: digitButton,
         operationButton: operationButton
     };
-};
+}
 
 const Keypad = () => {
-    let buttons = calculatorButtons.map((i, idx) => {
-        let btn = ButtonModule(idx, i.id, i.type, i.text);
-        if (i.type === 'number' || i.type === 'decimal') {
+    let buttons = calculatorButtons.map((item, idx) => {
+        let {id, type, text} = item;
+        let btn = ButtonModule(idx, id, type, text);
+        if (type === 'number' || type === 'decimal') {
             return btn.digitButton();
+        } else if (type === 'operation' || type === 'clear-all') {
+            return btn.operationButton();
         } else {
-            return <Button key={"btn-"+idx} id={i.id} type={i.type} text={i.text} />
+            return <Button key={"btn-"+idx} id={id} type={type} text={text} />
         }
     });
 
---
+++ src/containers/OperationButton.js
@@ -0,0 +1,85 @@
+import React from "react";
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { pressClear, pressOperation } from '../actions';
+import { isDigitAddable } from "../utils";
+
+const OperationButton = (props) => {
+    const {id, type, text} = props;
+    const {pressClear, pressOperation} = props;
+    const {formula, result, evaluated, previousResult} = props;
+
+    const handleButtonClick = (buttonType, buttonText) => {
+        switch(buttonType) {
+            case 'operation':
+                handleOperation(buttonText);
+                break;
+            case 'clear-all':
+                handleAC(buttonText);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const handleAC = () => {
+        pressClear({
+            formula: "",
+            evaluated: false,
+            previousResult: "",
+            result: ""
+        });
+    }
+
+    const handleOperation = (buttonText) => {
+        let userInput = buttonText;
+        let newFormula = formula.concat(userInput);
+
+        if(evaluated) {
+            pressOperation({
+                formula: previousResult + userInput,
+                result: userInput,
+                evaluated: false
+            });
+            return;
+        }
+
+        if(result === "" && (/[*/+]/).test(userInput)) {
+            newFormula = "0" + userInput;
+        }
+
+        if(userInput === "-") {
+            newFormula = newFormula.replace(/-{2}|\+-$/,userInput);
+        } else {
+            newFormula = newFormula.replace(/[*+\-/]{2,}$/,userInput);
+        }
+
+        pressOperation({
+            formula: newFormula,
+            result: userInput
+        });
+    }
+
+    return(
+        <button id={id} className={"btn " + type}
+            onClick={() => handleButtonClick(type, text)}
+        >{text}</button>
+    );
+}
+
+const mapStateToProps = (state) => ({
+    formula: state.formula,
+    result: state.result,
+    evaluated: state.evaluated,
+    previousResult: state.previousResult
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    pressClear: (obj) => dispatch(pressClear(obj)),
+    pressOperation: (obj) => dispatch(pressOperation(obj))
+});
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(OperationButton);
